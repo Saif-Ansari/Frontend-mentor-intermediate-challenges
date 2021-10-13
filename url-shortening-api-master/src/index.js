148f63156d76c7de1427401312cee33f81e9a639
Saif-Ansari <sfansari007@gmail.com>
2021-10-13 22:46:23 +0530

    added basic validation functionality and handled shorten url blocks:

--- url-shortening-api-master/src/index.js
+++ url-shortening-api-master/src/index.js
@@ -1,6 +1,8 @@
 import "./style.scss";
 let token = "8c2bde14e18cbf44c81a02258f061159bd67abb0";
 let urlData = [];
+let input = document.querySelector(".url");
+let errMsg = document.querySelector(".errMsg");
 
 function fetchData(url) {
   let data = {
@@ -17,12 +19,43 @@ function fetchData(url) {
   })
     .then((response) => response.json())
     .then((data) => {
-      urlData.push({ long_url: url, short_url: data.link });
+      if (data.message) {
+        input.style.border = "2px solid red";
+        errMsg.style.display = "block";
+      } else {
+        if (urlData.length === 3) {
+          urlData.shift();
+        }
+        urlData.push({ long_url: url, short_url: data.link });
+        renderUrl();
+      }
     });
-  console.log({ urlData });
 }
 
 document.getElementById("getLink").addEventListener("click", function (e) {
-  let inputValue = document.querySelector(".url").value;
-  fetchData(inputValue);
+  let inputValue = input.value;
+  if (!inputValue.trim()) {
+    input.style.border = "2px solid red";
+    errMsg.style.display = "block";
+    return;
+  } else {
+    fetchData(inputValue);
+    input.style.border = "none";
+    errMsg.style.display = "none";
+  }
 });
+
+function renderUrl() {
+  document.querySelector(".urlBlock").innerHTML = "";
+  urlData.map(({ long_url, short_url }) => {
+    let data = `<p>${long_url}</p>
+    <div>
+      <span>${short_url}</span>
+      <button>Copy</button>
+    </div>`;
+    let div = document.createElement("div");
+    div.className = "urlBlock__child";
+    div.innerHTML = data;
+    document.querySelector(".urlBlock").appendChild(div);
+  });
+}
